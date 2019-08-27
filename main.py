4d601c15dcbef194ada402d08b65feb974b47d82
Prateek Vachher <vachh007@umn.edu>
2019-08-27 12:02:31 +0530

    File Update

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from imagescrapper import download_images
+from train_model import train_model
+# download_images("cats", "Animals")
 
-download_images("dogs", "Animals")
+
+train_model("model_definition.yaml", ".","csvfile.csv", "TestExp", "log.txt")
